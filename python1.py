92fb9dbc179e9d4acefbb63100c0105cf65da623
gayathree <gnetha30@gmail.com>
2023-01-16 14:57:42 +0530

    file added

---
+++ python1.py
@@ -0,0 +1,69 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 11,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "square of the num are: [4, 16, 36, 64, 100]\n"
+     ]
+    }
+   ],
+   "source": [
+    "list=[2,4,6,8,10]\n",
+    "square=[i**2 for i in list]\n",
+    "print('square of the num are:',square)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 12,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "cube of the num are: [8, 64, 216, 512, 1000]\n"
+     ]
+    }
+   ],
+   "source": [
+    "cube=[i**3 for i in list]\n",
+    "print('cube of the num are:',cube)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
